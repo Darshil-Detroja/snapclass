266edce216a9d7ed6d521f5aac27c69aa9a44315
Darshil Detroja <darshildetroja@gmail.com>
2026-06-20 22:39:46 +0530

    Chnage logo url

--- src/components/footer.py
+++ src/components/footer.py
@@ -2,11 +2,11 @@ import streamlit as st
 
 
 def footer_home():
-    logo_url = "https://i.ibb.co/4r5X1FY/apnacollege.png"
+    logo_url = "assets/logo.png"
     
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
-        <p style="font-weight:bold; color:white;"> Created with ❤️ by </p>  
+        <p style="font-weight:bold; color:white;"> Created ❤️ by </p>  
         <img src='{logo_url}' style='max-height:25px' />
         </div>
                 
@@ -14,7 +14,7 @@ def footer_home():
 
 
 def footer_dashboard():
-    logo_url = "https://i.ibb.co/4r5X1FY/apnacollege.png"
+    logo_url = "assets/logo.png"
     
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
